dfb637d4c0ee29604d13071763860b781b76a196
aakrut <aakrutdabhi@gmail.com>
2022-12-31 11:27:38 +0530

    add tailwindcss

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-
 const Home: NextPage = () => {
   return (
     <div>
-    
+      Decentragram
     </div>
   )
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ["poppins"],
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "768px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+      ssm: { max: "290px" },
+    },
+  },
+  plugins: [],
+};
