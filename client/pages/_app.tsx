5aee4e9fe14113e256956918058e6a345c1f885b
aakrut <aakrutdabhi@gmail.com>
2023-02-10 22:13:08 +0530

    add wrapper

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,23 +2,26 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import BundlrContextProvider from "../context/bundlrContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
+      <BundlrContextProvider>
+        <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </BundlrContextProvider>
     </div>
   );
 }
