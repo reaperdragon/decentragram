a40238c73ba4238148a55052eeef781dbfea0a38
aakrut <aakrutdabhi@gmail.com>
2023-02-11 19:29:41 +0530

    add client

---
+++ client/client.ts
@@ -0,0 +1,9 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+  cache: new InMemoryCache(),
+});
